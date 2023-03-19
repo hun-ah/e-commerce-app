7f27bdf7e51362e87fc7a8ca4e3efd6ce063fc89
Hannah Wohl-Machado <hannahmachado@yahoo.ca>
2023-03-19 13:15:13 -0400

    fixed homepage view on desktop

--- src/components/NewCollection.jsx
+++ src/components/NewCollection.jsx
@@ -1,12 +1,22 @@
 import styled from 'styled-components'
 import { homepageImgs } from '../data/homepage'
 
-const Section = styled.section`
-   font-family: 'Poppins', sans-serif;
-   height: 100vh;
+const Container = styled.div`
    width: 100%;
    display: flex;
-   margin: 20px 0;
+   align-items: center;
+   justify-content: center;
+`
+
+const Section = styled.section`
+   font-family: 'Poppins', sans-serif;
+   width: 100%;
+   display: flex;
+   margin: 6px 0;
+
+   @media screen and (min-width: 1500px){
+      width: 1500px;
+   }
 `
 
 const Left = styled.div`
@@ -35,8 +45,10 @@ const Right = styled.div`
 const ImgContainer = styled.div`
    background-image: url(${homepageImgs[1].img});
    background-position: center;
+   background-repeat: no-repeat;
+   background-size: cover;
    width: 100%;
-   height: 100%;
+   height: 750px;
    cursor: pointer;
 `
 const SloganContainer = styled.div`
@@ -53,18 +65,20 @@ const ShopNow = styled.a`
 
 const NewCollection = () => {
    return (
-      <Section>
-         <Left>
-            <ImgContainer></ImgContainer>
-         </Left>
-         <Right>
-            <h2>NEW AROUND HERE</h2>
-            <SloganContainer>
-               <p>New styles and colours for your spring debut. Flowers aren't the only thing in bloom.</p>
-               <ShopNow>SHOP NOW</ShopNow>
-            </SloganContainer>
-         </Right>
-      </Section>
+      <Container>
+         <Section>
+            <Left>
+               <ImgContainer></ImgContainer>
+            </Left>
+            <Right>
+               <h2>NEW AROUND HERE</h2>
+               <SloganContainer>
+                  <p>New styles and colours for your spring debut. Flowers aren't the only thing in bloom.</p>
+                  <ShopNow>SHOP NOW</ShopNow>
+               </SloganContainer>
+            </Right>
+         </Section>
+      </Container>
    )
 }
 
