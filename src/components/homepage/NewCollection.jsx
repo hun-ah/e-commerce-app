e5cf705fd8262198aa7fbc2604d7526d2e8af238
Hannah Wohl-Machado <hannahmachado@yahoo.ca>
2023-03-25 13:04:05 -0400

    layout edits to newCollection

--- src/components/homepage/NewCollection.jsx
+++ src/components/homepage/NewCollection.jsx
@@ -15,6 +15,7 @@ const Section = styled.section`
    width: 100%;
    display: flex;
    margin: 60px 0;
+   margin-bottom: 100px;
 
 
    @media screen and (min-width: 1500px){
@@ -31,17 +32,20 @@ const Left = styled.div`
 const Right = styled.div`
    display: flex;
    flex-direction: column;
+   color: #2D2B2B;
    gap: 200px;
    width: 50%;
    height: 100%;
    padding: 20px;
 
    & h2 {
-      font-size: 18px;
+      font-size: 16px;
    }
 
    & p {
-      font-size: 54px;
+      font-size: 46px;
+      line-height: 125%;
+      letter-spacing: -0.03em;
    }
 `
 
@@ -58,16 +62,31 @@ const SloganContainer = styled.div`
    flex-direction: column;
    gap: 30px;
 `
-const ShopNow = styled.span`
-   cursor: pointer;
-   width: fit-content;
+const ShopNow = styled.button`
+   font-family: 'Poppins', sans-serif;
+   background: #F9F9F9;
    font-size: 14px;
-   border-bottom: 1px solid black;
+   padding: 16px 32px;
+   margin-top: 40px;
+   width: 160px;
+   height: 56px;
+   border: 1px solid #2D2B2B;
+   line-height: 24px;
+   color: #2D2B2B;
+   cursor: pointer;
+   transition: .1s ease -in -out;
+
+   &:hover {
+   border-color: #888585;
+   color: #888585;
+   text-decoration: underline;
+}
 `
 
 const StyledLink = styled(Link)`
      text-decoration: none;
-     color: #000;
+   color: #2D2B2B;
+
 `
 
 const NewCollection = ({ setFilters }) => {
@@ -89,7 +108,7 @@ const NewCollection = ({ setFilters }) => {
                <SloganContainer>
                   <p>New styles and colours for your spring debut. Flowers aren't the only thing in bloom.</p>
                   <StyledLink to='/productList/all' onClick={() => goToProductPage ? setFilters({}) : ''}>
-                     <ShopNow>SHOP NOW</ShopNow>
+                     <ShopNow>Shop spring</ShopNow>
                   </StyledLink>
                </SloganContainer>
             </Right>
