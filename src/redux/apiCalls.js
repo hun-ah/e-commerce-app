c4cebc2e829e3c0fc194cffbcd830f0e883182ab
Hannah Wohl-Machado <hannahmachado@yahoo.ca>
2023-03-31 15:52:26 -0400

    js files for log in logic

---
+++ src/redux/apiCalls.js
@@ -0,0 +1,19 @@
+import { loginFailure, loginStart, loginSuccess } from "./userRedux"
+
+export const login = async (dispatch, user) => {
+   dispatch(loginStart())
+   try {
+      const res = await fetch('http://localhost:5000/api/login', {
+         method: 'POST',
+         headers: {
+            'Content-Type': 'application/json'
+         },
+         body: JSON.stringify(user)
+      })
+      const data = await res.json()
+      console.log(data)
+      dispatch(loginSuccess(data))
+   } catch (err) {
+      dispatch(loginFailure())
+   }
+}
