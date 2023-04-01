e2a04ec369ad8fc0b84b2601ef399677c8816a7a
Hannah Wohl-Machado <hannahmachado@yahoo.ca>
2023-04-01 14:28:03 -0400

    moved state for tokens up to App level

--- src/App.jsx
+++ src/App.jsx
@@ -14,16 +14,19 @@ import { useState } from 'react'
 function App() {
   const [quantity, setQuantity] = useState(1)
   const [filters, setFilters] = useState({})
+  const [stripeToken, setStripeToken] = useState(null)
+  // used for comparing against stripeToken when going back to checkout page
+  const [tokenForCompare, setTokenForCompare] = useState(null)
 
   return (
     <Routes>
       <Route path="/" element={<Homepage setFilters={setFilters} />} />
       <Route path="/productList/:category" element={<ProductList filters={filters} setFilters={setFilters} />} />
       <Route path="/ourStory" element={<OurStory setFilters={setFilters} />} />
-      <Route path="/shoppingBag" element={<ShoppingBag setFilters={setFilters} quantity={quantity} setQuantity={setQuantity} />} />
+      <Route path="/shoppingBag" element={<ShoppingBag setFilters={setFilters} quantity={quantity} setQuantity={setQuantity} stripeToken={stripeToken} setStripeToken={setStripeToken} tokenForCompare={tokenForCompare} setTokenForCompare={setTokenForCompare} />} />
       <Route path="/product/:id" element={<Product setFilters={setFilters} quantity={quantity} />} />
       <Route path="*" element={<NotFound />} />
-      <Route path="/success" element={<PaymentSuccess />} />
+      <Route path="/success" element={<PaymentSuccess stripeToken={stripeToken} setStripeToken={setStripeToken} setTokenForCompare={setTokenForCompare} setFilters={setFilters} />} />
     </Routes>
   );
 }
