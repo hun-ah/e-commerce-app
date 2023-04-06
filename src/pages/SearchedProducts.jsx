1e70d8ab2eda6c94e6ae5210eebcfb9dc594f0de
Hannah Wohl-Machado <hannahmachado@yahoo.ca>
2023-04-06 10:25:32 -0400

    layout and styling for search pages

--- src/pages/SearchedProducts.jsx
+++ src/pages/SearchedProducts.jsx
@@ -3,16 +3,88 @@ import Navbar from "../components/Navbar"
 import Spacer from "../components/Spacer"
 import Footer from "../components/Footer"
 import ProductCard from "../components/product-list/ProductCard"
+import CategorySlider from "../components/homepage/CategorySlider"
+import { AiOutlineSearch } from 'react-icons/ai'
+
+const Container = styled.div`
+   width: 100%;
+   min-height: 100vh;
+   display: flex;
+   flex-direction: column;
+   color: #2d2b2b;
+   font-family: 'Poppins', sans-serif;
+`
+
+const ProductSection = styled.section`
+   width: 100%;
+   display: flex;
+   flex-direction: column;
+   margin: 0 auto;
+
+   @media screen and (min-width: 1500px){
+      width: 1500px;
+   }
+`
+
+const Products = styled.div`
+   display: flex;
+   flex-wrap: wrap;
+   justify-content: flex-start;
+   row-gap: 40px;
+   column-gap: 16px;
+   padding: 20px 20px;
+   width: 100%;
+   margin-top: 104px;
+`
+
+const Results = styled.div`
+   position: absolute;
+   top: 50px;
+   left: 20px;
+   font-size: 24px;
+   line-height: 36px;
+`
+
+const NoResults = styled.div`
+   display: flex;
+   gap: 14px;
+   width: 100%;
+`
+
+const SearchMsg = styled.h1`
+   font-size: 24px;
+   line-height: 36px;
+   flex: 1;
+`
 
 const SearchedProducts = ({ searched, setSearched, setFilters }) => {
-   console.log(searched)
    return (
       <>
          <Navbar setFilters={setFilters} searched={searched} setSearched={setSearched} />
          <Spacer />
-         {Array.isArray(searched) ? searched.map(obj => {
-            return <ProductCard key={obj._id} id={obj._id} title={obj.title} price={obj.price} category={obj.category} img={obj.img} size={obj.size} />
-         }) : <h1>{searched.msg}</h1>}
+         <Container>
+            <ProductSection>
+               {Array.isArray(searched) ?
+                  <div style={{ position: 'relative', marginTop: '50px', marginBottom: '80px' }}>
+                     <Results>{`Showing results for "${''}"`}</Results>
+                     <Products>
+                        {searched.map(obj => {
+                           return <ProductCard key={obj._id} id={obj._id} title={obj.title} price={obj.price} category={obj.category} img={obj.img} size={obj.size} />
+                        })}
+                     </Products>
+                  </div> :
+                  <Products>
+                     <NoResults>
+                        <AiOutlineSearch style={{ height: '25px', width: '25px', marginTop: '5px' }} />
+                        < SearchMsg >
+                           {searched.msg}
+                        </SearchMsg>
+                     </NoResults>
+                  </Products>
+               }
+            </ProductSection>
+            {!Array.isArray(searched) && <CategorySlider setFilters={setFilters} />}
+         </Container >
          <Footer />
       </>
    )
