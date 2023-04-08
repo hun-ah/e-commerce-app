f29366425fb4b8e943b0d08781aad766f289ff2e
Hannah Wohl-Machado <hannahmachado@yahoo.ca>
2023-04-08 17:32:44 -0400

    starting loading logic

--- src/redux/searchRedux.js
+++ src/redux/searchRedux.js
@@ -3,11 +3,16 @@ import { createSlice } from '@reduxjs/toolkit'
 const searchSlice = createSlice({
    name: 'search',
    initialState: {
-      search: ''
+      search: '',
+      isLoading: true
    },
    reducers: {
       searchInput: (state, action) => {
          state.search = action.payload
+         state.isLoading = true
+      },
+      loadingFalse: (state) => {
+         state.isLoading = false
       }
    }
 })
